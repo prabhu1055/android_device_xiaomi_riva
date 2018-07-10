2acfbdc1422f6e25dd5336830b3f569d116d2657
Danny Baumann <dannybaumann@web.de>
2018-07-10 09:09:29 +0200

    riva: Update call recording overlay.
    Change-Id: I3428fa6e47318b195a8dc46731e0aabc14a9a8e7

--- device.mk
+++ device.mk
@@ -21,6 +21,7 @@ $(call inherit-product, vendor/nitrogen/config/qcom_utils.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
