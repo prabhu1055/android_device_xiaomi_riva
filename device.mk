4427a9818a0376baa072a5059257462ca6e9013d
sunnyraj84348 <sunnyraj84348@gmail.com>
2018-09-23 10:50:36 +0000

    riva: Remove qcom utils path

--- device.mk
+++ device.mk
@@ -17,7 +17,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 $(call inherit-product, vendor/xiaomi/riva/riva-vendor.mk)
-$(call inherit-product, vendor/nitrogen/config/qcom_utils.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
